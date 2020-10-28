450ba869427f84b3d32f2629987b0ea1ef35d645
moldabayev.v <moldabayev.v@chocolife.kz>
2020-10-28 17:54:08 +0600

    support laravel 8

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+/** @psalm-suppress UndefinedFunction */
+
 use Illuminate\Support\Facades\Route;
 
 /** @psalm-suppress UndefinedFunction */
-
 Route::get(
     config('healthcheck.routesimple', '/health'),
     'Chocofamilyme\LaravelHealthCheck\Controllers\HealthCheckController@simple'
